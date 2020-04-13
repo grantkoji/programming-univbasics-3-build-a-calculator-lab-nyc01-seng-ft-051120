75f67472f9fc4dbaf44c3e216cb8bc197f22b34b
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-13 21:38:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  minus = num1 - num2
+  puts minus
 end
 
 def division(num1, num2)
-
+  div = num1 / num2
+  puts div
 end
 
 def multiplication(num1, num2)
-
+  multi = num1 * num2
+  puts multi
 end
 
 def modulo(num1, num2)
