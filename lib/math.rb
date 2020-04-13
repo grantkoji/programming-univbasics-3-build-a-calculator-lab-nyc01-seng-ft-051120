b0710f00b69e37067adf2742f432bed6442058d0
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-13 21:46:47 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  return remainder
 end
 
 def square_root(num)
-
+  squarert = Mathsqrt(num)
+  return squarert
 end
